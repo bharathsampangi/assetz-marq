4af4a59ab2e4d6dcc3f25d7e15e86462684f7c90
bharathsampangi <bharathsampangi95@gmail.com>
2022-08-06 14:11:26 +0530

    changes

---
+++ src/components/Amenities/Amenities.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles/Amenities.module.css"
+
+const Amenities = () => {
+    return (
+        <div className={styles.container}>
+            <h2>Amenities</h2>
+        </div>
+    )
+}
+
+ export default Amenities
